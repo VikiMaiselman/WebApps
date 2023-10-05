03aeacca8e057504256f83728172a360e56d9ab9
VikiMaiselman <maiselmanwikki@gmail.com>
2023-10-05 14:57:56 +0300

    give another port to the app

--- UV Index/index.js
+++ UV Index/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 let safeExposureTime;
 let renderData;
 let weatherCode;
